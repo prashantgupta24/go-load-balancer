2e4c4933b5744267d9df80680e1768f0194fdc2d
Prashant Gupta <prashantgupta@us.ibm.com>
2018-07-16 16:05:06 -0700

    new approach

--- main.go
+++ main.go
@@ -2,57 +2,46 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 type Request struct {
-	//make()
-	// getFn()
-	// getResChan()
 	fn     interface{}
 	result interface{}
 }
 
-type IntReq struct {
-	fn     func() int
-	result chan int
-}
-
-// func (r *IntReq) getFn() {
-// 	return r.fn
-// }
-
-type StringReq struct {
-	fn     func() string
-	result chan string
-}
-
-// func (r *Request) IntReq(fn func() int, result chan int){
-// 	fn     func() int
-// 	result chan int
-// }
 type LoadBalancer struct {
 }
 
-func (l *LoadBalancer) start(RequestChan chan Request) {
-	for {
-		select {
-		case r := <-RequestChan:
-			{
-				go worker(r)
-			}
-		}
+func (l *LoadBalancer) start(RequestChan *chan Request) {
+	for r := range *RequestChan {
+		go worker(r)
 	}
+	fmt.Println("Closing channel")
+}
+
+func sendTask(RequestChan chan Request, wg *sync.WaitGroup, request *Request) {
+	RequestChan <- *request
+	go func(request *Request) {
+		switch request.result.(type) {
+		case chan int:
+			ResChan := request.result.(chan int)
+			fmt.Println(<-ResChan)
+		case chan string:
+			ResChan := request.result.(chan string)
+			fmt.Println(<-ResChan)
+		}
+		wg.Done()
+	}(request)
 }
 
 func worker(request Request) {
-	//request.result <- request.fn()
 	switch request.result.(type) {
 	case chan int:
 		ResChan := request.result.(chan int)
 		fn := request.fn.(func() int)
 		ResChan <- fn()
-		fmt.Println("spinnig a worker!")
 	case chan string:
 		ResChan := request.result.(chan string)
 		fn := request.fn.(func() string)
@@ -63,30 +52,21 @@ func main() {
 	fmt.Println("Starting")
 
 	RequestChan := make(chan Request)
-
+	var wg sync.WaitGroup
 	loadBalancer := LoadBalancer{}
-	go loadBalancer.start(RequestChan)
 
-	//var req1 Request
+	go loadBalancer.start(&RequestChan)
 
 	req1 := Request{
 		fn: func() int {
-			time.Sleep(time.Second * 4)
+			time.Sleep(time.Second)
 			return 2 * 4
 		},
 		result: make(chan int),
 	}
 
-	RequestChan <- req1
-
-	switch req1.result.(type) {
-	case chan int:
-		ResChan := req1.result.(chan int)
-		fmt.Println(<-ResChan)
-	case chan string:
-		ResChan := req1.result.(chan string)
-		fmt.Println(<-ResChan)
-	}
+	wg.Add(1)
+	go sendTask(RequestChan, &wg, &req1)
 
 	req2 := Request{
 		fn: func() string {
@@ -95,15 +75,9 @@ func main() {
 		result: make(chan string),
 	}
 
-	RequestChan <- req2
+	wg.Add(1)
+	go sendTask(RequestChan, &wg, &req2)
 
-	switch req2.result.(type) {
-	case chan int:
-		ResChan := req2.result.(chan int)
-		fmt.Println(<-ResChan)
-	case chan string:
-		ResChan := req2.result.(chan string)
-		fmt.Println(<-ResChan)
-	}
-
+	wg.Wait()
+	close(RequestChan)
 }
