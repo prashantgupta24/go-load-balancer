26655ca2a34c5a776e487ddae92be4369a335a60
Prashant Gupta <prashantgupta@us.ibm.com>
2018-07-16 15:45:08 -0700

    new approach

--- main.go
+++ main.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 type Request struct {
+	//make()
+	// getFn()
+	// getResChan()
+	fn     interface{}
+	result interface{}
+}
+
+type IntReq struct {
 	fn     func() int
 	result chan int
 }
 
+// func (r *IntReq) getFn() {
+// 	return r.fn
+// }
+
+type StringReq struct {
+	fn     func() string
+	result chan string
+}
+
+// func (r *Request) IntReq(fn func() int, result chan int){
+// 	fn     func() int
+// 	result chan int
+// }
 type LoadBalancer struct {
 }
 
@@ -24,7 +46,18 @@ func (l *LoadBalancer) start(RequestChan chan Request) {
 }
 
 func worker(request Request) {
-	request.result <- request.fn()
+	//request.result <- request.fn()
+	switch request.result.(type) {
+	case chan int:
+		ResChan := request.result.(chan int)
+		fn := request.fn.(func() int)
+		ResChan <- fn()
+		fmt.Println("spinnig a worker!")
+	case chan string:
+		ResChan := request.result.(chan string)
+		fn := request.fn.(func() string)
+		ResChan <- fn()
+	}
 }
 func main() {
 	fmt.Println("Starting")
@@ -34,14 +67,43 @@ func main() {
 	loadBalancer := LoadBalancer{}
 	go loadBalancer.start(RequestChan)
 
-	r1 := Request{
+	//var req1 Request
+
+	req1 := Request{
 		fn: func() int {
+			time.Sleep(time.Second * 4)
 			return 2 * 4
 		},
 		result: make(chan int),
 	}
 
-	RequestChan <- r1
-	fmt.Println(<-r1.result)
+	RequestChan <- req1
+
+	switch req1.result.(type) {
+	case chan int:
+		ResChan := req1.result.(chan int)
+		fmt.Println(<-ResChan)
+	case chan string:
+		ResChan := req1.result.(chan string)
+		fmt.Println(<-ResChan)
+	}
+
+	req2 := Request{
+		fn: func() string {
+			return "hello"
+		},
+		result: make(chan string),
+	}
+
+	RequestChan <- req2
+
+	switch req2.result.(type) {
+	case chan int:
+		ResChan := req2.result.(chan int)
+		fmt.Println(<-ResChan)
+	case chan string:
+		ResChan := req2.result.(chan string)
+		fmt.Println(<-ResChan)
+	}
 
 }
